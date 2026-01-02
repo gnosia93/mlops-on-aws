56d146ff55f7ccc5b559e6f9612d37118ff356fb
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-03 00:25:44 +0900

    Update hello-airflow-dag.py

--- dags/hello-airflow-dag.py
+++ dags/hello-airflow-dag.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 # 1. DAG 인스턴스 정의 (스케줄링 및 기본 설정)
 with DAG(
-    dag_id='hello_airflow', # DAG의 고유 ID
-    start_date=datetime(2025, 1, 1), # DAG가 처음 실행될 날짜
-    schedule='@daily', # 매일 실행되도록 설정
-    catchup=False, # start_date와 현재 날짜 사이의 누락된 DAG 실행을 건너뜀
-    tags=['simple', 'hello'] # DAG를 분류하기 위한 태그 (선택 사항)
+    dag_id='hello_airflow',                         # DAG의 고유 ID
+    start_date=datetime(2026, 1, 1),                # DAG가 처음 실행될 날짜
+    schedule='@daily',                              # 매일 실행되도록 설정
+    catchup=False,                                  # start_date와 현재 날짜 사이의 누락된 DAG 실행을 건너뜀
+    tags=['simple', 'hello']                        # DAG를 분류하기 위한 태그 (선택 사항)
 ) as dag:
 
     # 작업을 수행할 Python 함수 정의
